d19ecc3177616f9f7bebf715d0cb0b726bb3870e
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-10 16:51:25 +0530

    abstract base pipeline class definition and sample implementation

--- 02_codes/pipeLine.py
+++ 02_codes/pipeLine.py
@@ -1,30 +1,51 @@
-# If i want to make a pipeline object in Python - A class that can act as a pipeline given a bunch of tasks like column manipulation, column transformation, model build etc - How to go about building it
-
-# For example - My aim is to chain a few commonly used data cleaning tasks together and make them run as a pipeline. The simple tasks all have code - I want you to design a pipeline class that takes these as inputs and executes them in order. Need an abstract base class that can furher be inherited from
+# If i want to make a pipeline object in Python - A class that can act as a
+# pipeline given a bunch of tasks like column manipulation, column
+# transformation, model build etc - How to go about building it?
+# For example - My aim is to chain a few commonly used data cleaning tasks
+# together and make them run as a pipeline. The simple tasks all have code - I
+# want you to design a pipeline class that takes these as inputs and executes
+# them in order. Need an abstract base class that can furher be inherited from
 
 from abc import ABC, abstractmethod
 
-class Parent(ABC):
+class PipeLine(ABC):
     @abstractmethod
-    def methodOne(self):
-        print("Base methodOne Called")
+    def dataImport(self):
+        raise NotImplementedError("dataImport() not defined in subclass")
 
-    # @abstractmethod
-    def methodTwo(self):
-        print("Base methodTwo Called")
+    @abstractmethod
+    def dataExport(self):
+        raise NotImplementedError("dataExport() not defined in subclass")
 
-class Son(Parent):
-    def methodOne(self):
-        print("Child methodOne Called")
+    def dataTransform(self):
+        pass
 
-    # def methodTwo(self):
-    #     return super().methodTwo()
+    def featureEngineering(self):
+        pass
+
+class RandomForestClassification(PipeLine):
+    def dataImport(self):
+        print("Sub Class Method Data Import Called")
+
+    def dataExport(self):
+        print("Sub Class Method Data Export Called")
+
+    # def dataTransform(self):
+    #     return super().dataTransform()
+
+    # def featureEngineering(self):
+    #     return super().featureEngineering()
+
+
 
 def main():
-    # obj = Parent()
-    obj = Son()
-    obj.methodOne()
-    obj.methodTwo()
+    # Create a RandomForestClassification object which follows the properties
+    # defined by Abstract SuperClass PipeLine
+    obj = RandomForestClassification()
+    obj.dataImport()
+    obj.dataExport()
+    obj.dataTransform()
+    obj.featureEngineering()
 
 if __name__ == '__main__':
     main()
